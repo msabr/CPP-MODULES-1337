0cf27af485cb7d8059b943a52d02a823cba227c0
Mohamed Sabr <msabr@e3r1p15.1337.ma>
2026-02-15 14:21:48 +0000

    ex00

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -6,27 +6,29 @@
 /*   By: msabr <msabr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 15:01:31 by msabr             #+#    #+#             */
-/*   Updated: 2026/02/10 15:48:19 by msabr            ###   ########.fr       */
+/*   Updated: 2026/02/15 11:47:50 by msabr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-int Bureaucrat::getGrade()
-{
-    return this->grade;
-}
-
 Bureaucrat::Bureaucrat() : name("default"), grade(150)
 {
-    std::cout << "Default Constructor called" << std::endl;
+    std::cout << "Bureaucrat default constructor called" << std::endl;
 }
 
-
-Bureaucrat::Bureaucrat(const Bureaucrat &other)
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : name(other.name), grade(other.grade)
 {
-    *this = other;
-    std::cout << "Copy Constructor called" << std::endl;
+    std::cout << "Bureaucrat copy constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int &grade) : name(name), grade(grade)
+{
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    else if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+    std::cout << "Bureaucrat parameterized constructor called" << std::endl;
 }
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat &obj)
@@ -35,21 +37,33 @@ Bureaucrat& Bureaucrat::operator=(const Bureaucrat &obj)
     {
         this->grade = obj.grade;
     }
-    std::cout << "Copy Assignment Operator called" << std::endl;
+    std::cout << "Bureaucrat copy assignment operator called" << std::endl;
     return *this;
-}
-
-Bureaucrat::Bureaucrat(const std::string &name, int &grade) : name(name), grade(grade)
-{
-    if (grade < 1)
-        throw std::runtime_error("Grade too high");
-    else if (grade > 150)
-        throw std::runtime_error("Grade too low");
-    std::cout << "Parameterized Constructor called" << std::endl;
 }
 
 Bureaucrat::~Bureaucrat()
 {
-    std::cout << "Default Deconstructor called" << std::endl;
-    
+    std::cout << "Bureaucrat destructor called" << std::endl;
 }
+
+int Bureaucrat::getGrade() const
+{
+    return this->grade;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high!";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low!";
+}
+
+std::ostream &operator<<(std::ostream &out,const Bureaucrat &obj)
+{
+    out << obj.getGrade();
+    return out;
+}
+
--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: msabr <msabr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 14:44:35 by msabr             #+#    #+#             */
-/*   Updated: 2026/02/12 15:24:48 by msabr            ###   ########.fr       */
+/*   Updated: 2026/02/15 12:30:46 by msabr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,22 @@ class Bureaucrat
         Bureaucrat(const std::string &name, int &grade);
         Bureaucrat& operator=(const Bureaucrat &obj);
         ~Bureaucrat();
-        int getGrade();
+        int getGrade() const;
         class GradeTooHighException;
         class GradeTooLowException;
 };
 
 class Bureaucrat::GradeTooHighException : public std::exception 
 {
-    public: 
-        const char* what() const throw();
+    public:
+        virtual const char* what() const throw();
 };
 
-
+class Bureaucrat::GradeTooLowException : public std::exception 
+{
+    public:
+        virtual const char* what() const throw();
+};
 
 std::ostream &operator<<(std::ostream &out,const Bureaucrat &obj);
 
